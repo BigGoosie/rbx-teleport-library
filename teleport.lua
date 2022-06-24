3086254d603c0bcb354c7c328c3a3cabc7e7150b
BigGoosie <57724785+BigGoosie@users.noreply.github.com>
2022-06-24 13:54:36 -0700

    Update teleport.lua

--- teleport.lua
+++ teleport.lua
@@ -1,5 +1,4 @@
 local Teleport = {}
-Teleport.TweenAnim = nil
 Teleport.Style = {
 	Linear = 1,
 	Distance = 2,
@@ -25,11 +24,11 @@ end
 function Teleport:Advanced(...)
 	local lp = game.Players.LocalPlayer
 	local TweenService = game:GetService("TweenService")
+	local TweenAnim = nil
 	local params = {...}
 
 	if (lp.Character == nil) then return print("Cannot find localplayer") end
-	if (Teleport.TweenAnim ~= nil) then Teleport.TweenAnim:Cancel() end
-
+	if (TweenAnim ~= nil) then TweenAnim:Cancel() end
 
 	print(typeof(params[3]))
 	if (params[1] == Teleport.Type.Mouse) then 
@@ -40,23 +39,23 @@ function Teleport:Advanced(...)
 			if (params[3] ~= nil) then print("No need for this silly! It's automatic!") end
 
 			local Speed = Teleport:Distance(hit) / Teleport.Settings.CustomStudSpeed
-			Teleport.TweenAnim = TweenService:Create(
+			TweenAnim = TweenService:Create(
 				lp.Character.HumanoidRootPart,
 				TweenInfo.new(Speed, Enum.EasingStyle.Linear),
 				{ CFrame = CFrame.new(hit) }
 			)
-			Teleport.TweenAnim:Play(); Teleport.TweenAnim.Completed:Wait(); Teleport.TweenAnim = nil
+			TweenAnim:Play(); TweenAnim.Completed:Wait(); TweenAnim = nil
 		elseif (params[2] == Teleport.Style.Instant) then
 			if (params[3] ~= nil) then print("No need for this silly! It's instant!") end
 			lp.Character.HumanoidRootPart.CFrame = CFrame.new(hit)
 		else
 			if (params[3] == nil) then params[3] = 1.5 end
-			Teleport.TweenAnim = TweenService:Create(
+			TweenAnim = TweenService:Create(
 				lp.Character.HumanoidRootPart.CFrame,
 				TweenInfo.new(params[4], Enum.EasingStyle.Linear),
 				{ CFrame = CFrame.new(hit) }
 			)
-			Teleport.TweenAnim:Play(); Teleport.TweenAnim.Completed:Wait(); Teleport.TweenAnim = nil
+			TweenAnim:Play(); TweenAnim.Completed:Wait(); TweenAnim = nil
 		end
 	else
 		if (params[3] == nil) then return print("You need a CFrame or Instance to teleport to.") end
@@ -65,46 +64,46 @@ function Teleport:Advanced(...)
 				if (params[3] ~= nil) then print("No need for this silly! It's automatic!") end
 	
 				local Speed = Teleport:Distance(params[3]) / Teleport.Settings.CustomStudSpeed
-				Teleport.TweenAnim = TweenService:Create(
+				TweenAnim = TweenService:Create(
 					lp.Character.HumanoidRootPart,
 					TweenInfo.new(Speed, Enum.EasingStyle.Linear),
 					{ CFrame = params[3] }
 				)
-				Teleport.TweenAnim:Play(); Teleport.TweenAnim.Completed:Wait(); Teleport.TweenAnim = nil
+				TweenAnim:Play(); TweenAnim.Completed:Wait(); TweenAnim = nil
 			elseif (params[2] == Teleport.Style.Instant) then
 				if (params[3] ~= nil) then print("No need for this silly! It's instant!") end
 				lp.Character.HumanoidRootPart.CFrame = params[3]
 			else
 				if (params[4] == nil) then params[4] = 1.5 end
-				Teleport.TweenAnim = TweenService:Create(
+				TweenAnim = TweenService:Create(
 					lp.Character.HumanoidRootPart.CFrame,
 					TweenInfo.new(params[4], Enum.EasingStyle.Linear),
 					{ CFrame = params[3] }
 				)
-				Teleport.TweenAnim:Play(); Teleport.TweenAnim.Completed:Wait(); Teleport.TweenAnim = nil
+				TweenAnim:Play(); TweenAnim.Completed:Wait(); TweenAnim = nil
 			end
 		elseif (typeof(params[3]) == "Instance") then
 			if (params[2] == Teleport.Style.Distance) then 
 				if (params[3] ~= nil) then print("No need for this silly! It's automatic!") end
 	
 				local Speed = Teleport:Distance(params[3].CFrame) / Teleport.Settings.CustomStudSpeed
-				Teleport.TweenAnim = TweenService:Create(
+				TweenAnim = TweenService:Create(
 					lp.Character.HumanoidRootPart,
 					TweenInfo.new(Speed, Enum.EasingStyle.Linear),
 					{ CFrame = params[3].CFrame }
 				)
-				Teleport.TweenAnim:Play(); Teleport.TweenAnim.Completed:Wait(); Teleport.TweenAnim = nil
+				TweenAnim:Play(); TweenAnim.Completed:Wait(); TweenAnim = nil
 			elseif (params[2] == Teleport.Style.Instant) then
 				if (params[3] ~= nil) then print("No need for this silly! It's instant!") end
 				lp.Character.HumanoidRootPart.CFrame = params[3].CFrame
 			else
 				if (params[3] == nil) then params[3] = 1.5 end
-				Teleport.TweenAnim = TweenService:Create(
+				TweenAnim = TweenService:Create(
 					lp.Character.HumanoidRootPart.CFrame,
 					TweenInfo.new(params[4], Enum.EasingStyle.Linear),
 					{ CFrame = params[3].CFrame }
 				)
-				Teleport.TweenAnim:Play(); Teleport.TweenAnim.Completed:Wait(); Teleport.TweenAnim = nil
+				TweenAnim:Play(); TweenAnim.Completed:Wait(); TweenAnim = nil
 			end
 		else
 			print("Sorry, there is no support for: ".. typeof(params[3]).. " currently.")
@@ -114,10 +113,11 @@ end
 function Teleport:Smart(...)
 	local lp = game.Players.LocalPlayer
 	local TweenService = game:GetService("TweenService")
+	local TweenAnim = nil
 	local params = {...}
 
 	if (lp.Character == nil) then return print("Cannot find localplayer") end
-	if (Teleport.TweenAnim ~= nil) then Teleport.TweenAnim:Cancel() end
+	if (TweenAnim ~= nil) then TweenAnim:Cancel() end
 
 	if (params[3] == nil) then return print("You need a CFrame or Instance to teleport to.") end
 	if (typeof(params[3]) == "CFrame") then
@@ -126,24 +126,24 @@ function Teleport:Smart(...)
 		end
 
 		local Speed = Teleport:Distance(params[3]) / Teleport.Settings.CustomStudSpeed
-		Teleport.TweenAnim = TweenService:Create(
+		TweenAnim = TweenService:Create(
 			lp.Character.HumanoidRootPart,
 			TweenInfo.new(Speed, Enum.EasingStyle.Linear),
 			{ CFrame = params[3] }
 		)
-		Teleport.TweenAnim:Play(); Teleport.TweenAnim.Completed:Wait(); Teleport.TweenAnim = nil
+		TweenAnim:Play(); TweenAnim.Completed:Wait(); TweenAnim = nil
 	elseif (typeof(params[3]) == "Instance") then
 		if (Teleport:Distance(params[3].CFrame) <= Teleport.Settings.MinimumInstant and Teleport:Distance(params[3].CFrame) <= Teleport.Settings.MaximumInstantTeleport) then
 			lp.Character.HumanoidRootPart.CFrame = params[3].CFrame; return
 		end
 
 		local Speed = Teleport:Distance(params[3].CFrame) / Teleport.Settings.CustomStudSpeed
-		Teleport.TweenAnim = TweenService:Create(
+		TweenAnim = TweenService:Create(
 			lp.Character.HumanoidRootPart,
 			TweenInfo.new(Speed, Enum.EasingStyle.Linear),
 			{ CFrame = params[3].CFrame }
 		)
-		Teleport.TweenAnim:Play(); Teleport.TweenAnim.Completed:Wait(); Teleport.TweenAnim = nil
+		TweenAnim:Play(); TweenAnim.Completed:Wait(); TweenAnim = nil
 	else
 		print("Sorry, there is no support for: ".. typeof(params[3]).. " currently.")
 	end
