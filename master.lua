e0218cf7836956ed47937831ddefccd2aa5e75ab
BigGoosie <57724785+BigGoosie@users.noreply.github.com>
2022-07-11 18:32:20 -0700

    Update master.lua

--- master.lua
+++ master.lua
@@ -278,7 +278,7 @@ function Library.UI:CreateWindow()
         end)
 
         sectionClick.MouseButton1Click:Connect(function()
-            if (activeSection.Name == sectionFrame.Name) then return end
+            if (activeSection == sectionFrame) then return end
             tween = TweenService:Create(activeSection.sectionClick, TweenInfo.new(0.25, Enum.EasingStyle.Linear), {TextColor3 = Color3.fromRGB(150, 150, 150)}); tween:Play()
             for i, v in pairs(groupboxes:GetDescendants()) do
                 if (v.Parent.Name == sectionFrame.Name and v.Name == "groupbox") then 
@@ -303,7 +303,9 @@ function Library.UI:CreateWindow()
             groupbox.BackgroundColor3 = Color3.fromRGB(18, 18, 18)
             groupbox.BorderColor3 = Color3.fromRGB(47, 47, 47)
             groupbox.Size = UDim2.new(0, 100, 0, 100)
-        
+            groupbox.Visible = true
+            if (activeSection ~= sectionFrame) then groupbox.Visible = false end
+
             local gTextSize = Library.Utils:StringSize(gText, 13, Enum.Font.SourceSansBold)
             local groupboxTitle = Instance.new("TextLabel")
             groupboxTitle.Name = "groupboxTitle"
